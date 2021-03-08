e5057169c2852a75b6669a7d0193b76a18fc368d
Frantz680 <dubdub68250@hotmail.fr>
2021-03-08 18:15:16 +0100

    V0.20

--- src/purbeurre/views.py
+++ src/purbeurre/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def utilisateurs_views(request):
     # return render(request, "connexion.html")
     return render(request, 'utilisateurs/mon_compte.html')
