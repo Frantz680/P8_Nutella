1f52518ab446e2e037383703946927122df150f5
Frantz680 <dubdub68250@hotmail.fr>
2021-03-17 13:23:11 +0100

    V0.30

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import ModelForm
 
-from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control',
     }))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control',
     }))
+
+
+class UserForm(UserCreationForm):
+    name = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password1', 'password2']
+        
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from django.contrib.auth import views
-
 from .forms import LoginForm
+from accounts.views import utilisateurs_views, register
+
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True, authentication_form=LoginForm), name="login"),
     path('logout/', views.LogoutView.as_view(template_name='accounts/logout.html', next_page='/accounts/login/'), name="logout"),
+    path('utilisateurs/', utilisateurs_views, name='mon_compte'),
+    path('register/', register , name='register'),
     ]
